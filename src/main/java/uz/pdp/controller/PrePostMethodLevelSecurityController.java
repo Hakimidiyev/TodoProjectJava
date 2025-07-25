c5550e0d832c79e6841a3032a5cdd7f94b47ca88
pdp <hakim2803@gmail.com>
2025-07-25 14:31:32 +0500

    this project spring_security_pdp_homework

--- src/main/java/uz/pdp/controller/PrePostMethodLevelSecurityController.java
+++ src/main/java/uz/pdp/controller/PrePostMethodLevelSecurityController.java
@@ -1,12 +1,9 @@
 package uz.pdp.controller;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.security.RolesAllowed;
 
 @Controller
 @ResponseBody
