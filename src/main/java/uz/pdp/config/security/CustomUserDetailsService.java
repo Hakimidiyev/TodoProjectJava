8d72ebda7d1237830b24b66c13c956a749d31edc
pdp <hakim2803@gmail.com>
2025-07-25 14:10:09 +0500

    this project spring_security_pdp_homework

--- src/main/java/uz/pdp/config/security/CustomUserDetailsService.java
+++ src/main/java/uz/pdp/config/security/CustomUserDetailsService.java
@@ -1,8 +1,5 @@
 package uz.pdp.config.security;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
