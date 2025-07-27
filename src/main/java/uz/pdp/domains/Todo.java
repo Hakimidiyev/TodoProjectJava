1d4a1928a0273ecd2c0a63a681a202b7fe10dee9
pdp <hakim2803@gmail.com>
2025-07-27 12:14:26 +0500

    sessionUser_class getPrincipal create

--- src/main/java/uz/pdp/domains/Todo.java
+++ src/main/java/uz/pdp/domains/Todo.java
@@ -2,6 +2,7 @@ package uz.pdp.domains;
 
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,6 +14,7 @@ public class Todo {
     private Long id;
     private String title;
     private Integer priority;
+    private List<AuthUser> authUser;
     private LocalDateTime created_at;
 
 
